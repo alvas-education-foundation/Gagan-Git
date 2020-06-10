a68e34f81bfb5f965c86c9985ad6ae5e6c290dae
Gaganmkabadi <gagan.m.kabadi@gmail.com>
2020-06-10 14:59:11 +0530

    First

---
+++ 10 June 2020/MyClass.java
@@ -0,0 +1,13 @@
+package one;
+
+public class MyClass {
+	  static void myMethod() {
+	    System.out.println("I'm Good!");
+	  }
+
+	  public static void main(String[] args) {
+	    myMethod();
+	    myMethod();
+	    myMethod();
+	  }
+	}
