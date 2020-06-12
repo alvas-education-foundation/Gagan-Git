cae4489c26ee95cfcc800d750b3c63c84351e93a
Gaganmkabadi <gagan.m.kabadi@gmail.com>
2020-06-12 16:53:54 +0530

    First

---
+++ 12 June 2020/anonymous.java
@@ -0,0 +1,31 @@
+package one;
+
+
+
+//Java program to demonstrate need for Anonymous Inner class 
+
+
+//Java program to demonstrate Anonymous inner class 
+interface Age 
+{ 
+	int x = 21; 
+	void getAge(); 
+} 
+class anonymous
+{ 
+	public static void main(String[] args) { 
+
+		// Myclass is hidden inner class of Age interface 
+		// whose name is not written but an object to it 
+		// is created. 
+		Age oj1 = new Age() { 
+			@Override
+			public void getAge() { 
+				// printing age 
+				System.out.print("Age is "+x); 
+			} 
+		}; 
+		oj1.getAge(); 
+	} 
+} 
+
---
+++ 12 June 2020/lower.java
@@ -0,0 +1,29 @@
+package one;
+
+
+//Java program to demonstrate Lower Bounded Wildcards 
+import java.util.Arrays; 
+import java.util.List; 
+
+class lower 
+{ 
+	public static void main(String[] args) 
+	{ 
+		//Lower Bounded Integer List 
+		List<Integer> list1= Arrays.asList(4,5,6,7); 
+		
+		//Integer list object is being passed 
+		printOnlyIntegerClassorSuperClass(list1); 
+
+		//Number list 
+		List<Number> list2= Arrays.asList(4,5,6,7); 
+		
+		//Integer list object is being passed 
+		printOnlyIntegerClassorSuperClass(list2); 
+	} 
+
+	public static void printOnlyIntegerClassorSuperClass(List<? super Integer> list) 
+	{ 
+		System.out.println(list); 
+	} 
+} 
